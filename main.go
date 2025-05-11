335c7fd32978a116c063f6a4f220f9880561e4be
Mohammad Al-Sultani <mohammedalsultani@outlook.com>
2025-05-12 00:34:33 +0200

    add cleanInput func

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+func cleanInput(text string) []string {
+	var clean []string
+	return clean
+}
+
 func main() {
 	// This is a simple Go program that prints "Hello, World!" to the console.
 	println("Hello, World!")
